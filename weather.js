ce40218b0123c9fb99023c9be1672609994cfcf1
Lavanya5101 <164330683+Lavanya5101@users.noreply.github.com>
2024-03-21 15:22:39 +0530

    Create weather.js

---
+++ weather.js
@@ -0,0 +1,25 @@
+function getWeather() {
+    const locationInput = document.getElementById('locationInput').value;
+    const apiKey = 'YOUR_API_KEY'; 
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${locationInput}&appid=${apiKey}&units=metric`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            displayWeather(data);
+        })
+        .catch(error => {
+            console.log('Error fetching weather data:', error);
+        });
+}
+
+function displayWeather(data) {
+    const weatherInfo = document.querySelector('.weather-info');
+    weatherInfo.innerHTML = `
+        <p>Location: ${data.name}, ${data.sys.country}</p>
+        <p>Temperature: ${data.main.temp}°C</p>
+        <p>Weather: ${data.weather[0].main}</p>
+        <p>Humidity: ${data.main.humidity}%</p>
+        <p>Wind Speed: ${data.wind.speed} m/s</p>
+    `;
+}
